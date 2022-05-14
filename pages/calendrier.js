fcb608b3b8015db35669492cb8d7461ae60a8535
atodesco <todesco.alexandre34@gmail.com>
2022-05-14 18:52:36 +0200

    calendar work in progress

--- pages/calendrier.js
+++ pages/calendrier.js
@@ -5,6 +5,67 @@ import Logo from "../img/logo.png";
 import VraiLogo from "..//img/logo-original.jpg";
 import Image from "next/image";
 
+import { Calendar, dateFnsLocalizer } from "react-big-calendar";
+import format from "date-fns/format";
+import parse from "date-fns/parse";
+import startOfWeek from "date-fns/startOfWeek";
+import getDay from "date-fns/getDay";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import React, { useState } from "react";
+import DatePicker from "react-datepicker";
+import { endOfDay } from "date-fns";
+
+// const messages = {
+//   allDay: "journée",
+//   previous: "précédent",
+//   next: "suivant",
+//   today: "aujourd'hui",
+//   month: "mois",
+//   week: "semaine",
+//   day: "jour",
+//   agenda: "Agenda",
+//   date: "date",
+//   time: "heure",
+//   event: "événement", // Or anything you want
+//   showMore: (total) => `+ ${total} événement(s) supplémentaire(s)`,
+// };
+
+const messages = {
+  allDay: "journée",
+  Monday: "lundi",
+  previous: "précédent",
+  next: "suivant",
+  today: "aujourd'hui",
+  month: "mois",
+  week: "semaine",
+  day: "jour",
+  agenda: "Agenda",
+  date: "date",
+  time: "heure",
+  event: "événement", // Or anything you want
+  showMore: (total) => `+ ${total} événement(s) supplémentaire(s)`,
+};
+
+const locales = {
+  fr: require("date-fns/locale/fr"),
+};
+
+const localizer = dateFnsLocalizer({
+  format,
+  parse,
+  startOfWeek,
+  getDay,
+  locales,
+});
+
+const events = [
+  {
+    title: "Format1",
+    start: new Date(2022, 7, 0),
+    end: new Date(2022, 6, 0),
+  },
+];
+
 function CalendrierPage() {
   return (
     <div>
@@ -13,7 +74,16 @@ function CalendrierPage() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Navbar />
-      <div className="">CALENDRIER</div>
+      <div className="">
+        <Calendar
+          messages={messages}
+          localizer={localizer}
+          events={events}
+          startAccessor="start"
+          endAccessor="end"
+          style={{ height: 500, margin: "50px" }}
+        />
+      </div>
       <footer className="grid grid-cols-3 gap-1 place-items-stretch  bg-zinc-900">
         <div className="ml-6 my-12">
           <span className="text-2xl text-white font-small">
